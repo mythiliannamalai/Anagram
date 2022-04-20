3447bbb8c40f4b05a7266feb911e8b5e3099a5d2
mythili annamalai <mythiliannamalai45@gmail.com>
2022-04-20 10:05:25 +0530

    Check the two string

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace Workshop
+{
+    class Program
+    {
+        public void Display()
+        {
+            string val1 = "abcd";
+            string val2 = "dcba";
+            char[] a=val1.ToCharArray();
+            
+            if (val1.Contains(val2))
+            {
+                Console.WriteLine("It's anagram");
+            }
+            else
+            {
+                Console.WriteLine("It's not anagram");
+            }
+        }
+        static void Main(string[]args)
+        {
+            Program program = new Program();
+            program.Display();
+        }
+    }
+}
