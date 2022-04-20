b9d1beef94e6d029bf65084e3c2dbf3f29b6e8da
mythili annamalai <mythiliannamalai45@gmail.com>
2022-04-20 10:45:10 +0530

    anagram

--- Program.cs
+++ Program.cs
@@ -5,13 +5,34 @@ namespace Workshop
 {
     class Program
     {
-        public void Display()
+        public bool Display(string val1,string val2)
         {
-            string val1 = "abcd";
-            string val2 = "dcba";
-            char[] a=val1.ToCharArray();
+            if(val1.Length!=val2.Length)
+            {
+                return false;
+            }          
             
-            if (val1.Contains(val2))
+            char[] a=val1.ToLower().ToCharArray();
+            char[] b=val2.ToLower().ToCharArray();
+            Array.Sort(a);
+            Array.Sort(b);
+            for (int i = 0; i < a.Length; i++)
+            {
+                if(a[i].ToString() != b[i].ToString())
+                {
+                    return false;
+                }                
+            }
+            return true;           
+        }
+        static void Main(string[]args)
+        {
+            Program program = new Program();
+            Console.WriteLine("enter the val1:");
+            string val1 = Console.ReadLine();
+            Console.WriteLine("enter the val2 :");
+            string val2 = Console.ReadLine();            
+            if (program.Display(val1, val2) == true)
             {
                 Console.WriteLine("It's anagram");
             }
@@ -19,11 +40,6 @@ namespace Workshop
             {
                 Console.WriteLine("It's not anagram");
             }
-        }
-        static void Main(string[]args)
-        {
-            Program program = new Program();
-            program.Display();
         }
     }
 }
